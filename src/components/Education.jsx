bce076ee8476b64e02832e1a86579d2ccfcb854c
Shreya Goyal <shreyagoyal401@gmail.com>
2025-07-16 14:55:51 +0530

    education_section

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,13 +1,58 @@
+import React from 'react';
 import './style/Education.css';
 
-const Education = () => (
-    <ul className="timeline">
-      <li><strong>2022:</strong> Joined MITS Gwalior</li>
-      <li><strong>2023:</strong> Learned DSA, DBMS, OS</li>
-      <li><strong>2024:</strong> Internship + Projects</li>
-      <li><strong>2025:</strong> Final Year Work</li>
-    </ul>
+const Education = () => {
+  return (
+    <div className="education-container">
+      <div className="timeline-column">
+      <div className="edu-block top-left">
+          <div className="edu-grid">
+            <div className="edu-left">
+              <div className="degree-title">B.Tech, CSE</div>
+              <div className="college-name">Madhav Institute of Technology and Science, Deemed University, Gwalior</div>
+            </div>
+            <div className="edu-right">
+              <div className="edu-year">2024-28</div>
+              <div className="highlight">CGPA: 9.01</div>
+            </div>
+          </div>
+      </div>
+
+      <div className="edu-block bottom-middle">
+          <div className="edu-grid">
+            <div className="edu-left">
+              <div className="degree-title">Class XII (CBSE)</div>
+              <div className="college-name">St. Paul’s School, Morar, Gwalior</div>
+            </div>
+            <div className="edu-right">
+              <div className="edu-year">2023-24</div>
+              <div className="highlight">94.4%</div>
+            </div>
+          </div>
+      </div>
+
+
+      <div className="edu-block bottom-left">
+          <div className="edu-grid">
+            <div className="edu-left">
+              <div className="degree-title">Class X (CBSE)</div>
+              <div className="college-name">St. Paul’s School, Morar, Gwalior</div>
+            </div>
+            <div className="edu-right">
+              <div className="edu-year">2021-22</div>
+              <div className="highlight">95%</div>
+            </div>
+          </div>
+      </div>
+
+        <div className="timeline">
+          <span className="dot dot1"></span>
+          <span className="dot dot2"></span>
+          <span className="dot dot3"></span>
+        </div>
+      </div>
+    </div>
   );
-  
-  export default Education;
-  
+};
+
+export default Education;
